c577f197571040837280772c131186fa7f230cb2
Jeff Larson <thejefflarson@gmail.com>
2014-03-20 14:12:26 -0400

    it is the little things

---
+++ src/dht_bucket.c
@@ -0,0 +1,22 @@
+dht_bucket_t*
+dht_bucket_new() {
+  dht_bucket_t* bucket;
+  if((bucket = malloc(sizeof(dht_bucket_t))) == NULL)
+    return NULL;
+  return bucket;
+}
+
+dht_bucket_t*
+dht_bucket_insert(dht_bucket_t *root, dht_node_t *node) {
+
+}
+
+void
+dht_bucket_walk(dht_bucket_t *root, dht_baton_t *baton) {
+
+}
+
+void
+dht_bucket_free(dht_bucket_t *root) {
+
+}
