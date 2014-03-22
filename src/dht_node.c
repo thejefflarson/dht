a29d904507a22b897c3f52cdc74fa13af770f65f
Jeff Larson <thejefflarson@gmail.com>
2014-03-22 15:51:11 -0400

    fill out node

--- src/dht_node.c
+++ src/dht_node.c
@@ -1,13 +1,16 @@
 #include <memory.h>
 #include <time.h>
 #include "dht_node.h"
+#include "dht_util.h"
 
 dht_node_t *
-dht_node_new() {
+dht_node_new(char[160] id) {
   dht_node_t* node = (dht_node_t*) malloc(sizeof(dht_node_t));
+
   if(node == NULL)
     return null;
 
+  node->id = id;
   time(&node->created_at);
   time(&node->last_heard);
   return node;
