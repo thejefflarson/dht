097aea215a2ffdec976cd6391c574ba86b7ee958
Jeff Larson <thejefflarson@gmail.com>
2016-02-22 21:21:57 -0500

    one file, no waf, and test runner

--- src/dht_util.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-void
-dht_xor(uint8_t[32], uint8_t[32], uint8_t[32]);
-
-int
-dht_compare(uint8_t[32], uint8_t[32]);
--- test/test_dht.c
+++ test/test_dht.c
@@ -1,17 +1,17 @@
 #include "util.h"
-#include "dht_bucket.h"
+#include "../dht.c"
 #include "tap.h"
 
-static void
-test_find_node(){
-  dht_t *dht = dht_new();
-  ok(dht != NULL, "dht allocated correctly");
-  // todo
-  dht_free(dht);
-}
+// static void
+// test_find_node(){
+//   dht_t *dht = dht_new();
+//   ok(dht != NULL, "dht allocated correctly");
+//   // todo
+//   dht_free(dht);
+// }
 
-void
+int
 main(){
   start_test;
-  test_ping();
+  // test_ping();
 }
